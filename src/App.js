a14f1c3b09453c388e7d88b1d1260e87f17ad212
MyGitHubAccount10 <23025668@myrp.edu.sg>
2025-05-24 12:51:24 +0800

    Updated app.js to include login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ShoppingCartPage from './ShoppingCartPage';
+import LoginPage from './LoginPage';
 // import './index.css'; // Or your global stylesheet where Inter font might be imported
 
 function App() {
   return (
     <div className="App">
+      <LoginPage />
       <ShoppingCartPage />
     </div>
   );

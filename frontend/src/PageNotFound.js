0379612d718f927e9535c262c7a9e71e23ffb78f
Best-Jason <23022483@myrp.edu.sg>
2025-06-11 15:04:25 +0800

    Fixed PageNotFound (Jason, i know it shows my name, but incase you missed it, i've made and edited this file)

---
+++ frontend/src/PageNotFound.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Website.css';
+import Header from './Header';
+import Footer from './Footer';
+
+
+function OrderHistoryPage() {
+    return (
+        <>
+            <Header />
+            {/* --- Main Content --- */}
+            <div style={{alignItems: 'center', textAlign: 'center', padding: '20px'}}>
+                <h2>Page Not Found</h2>
+                <p>Try Another Link</p>
+
+            </div>
+            <Footer />
+        </>
+    );
+}
+
+export default OrderHistoryPage;
